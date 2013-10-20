a0454b6d8569fb8970bafb7d63b2add05d0ff393
Sean Griffin <sean@thoughtbot.com>
2013-10-20 15:30:52 -0600

    Set up api proxying

--- config/application.js
+++ config/application.js
@@ -16,13 +16,12 @@ module.exports = require(process.env['LINEMAN_MAIN']).config.extend('application
   // requests for paths that don't match a static asset in ./generated will be forwarded to
   // whatever service might be running on the specified port.
   //
-  // server: {
-  //   apiProxy: {
-  //     enabled: true,
-  //     host: 'localhost',
-  //     port: 3000
-  //   }
-  // }
+  server: {
+    apiProxy: {
+      enabled: true,
+      host: 'http://fowa-backbone.herokuapp.com/seang',
+    }
+  }
 
   // Sass
   //
