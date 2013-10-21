247f57d11b5f2819c0d737da4d300231cc596568
Sean Griffin <sean@thoughtbot.com>
2013-10-20 20:16:18 -0600

    Fix cross domain

--- app/js/app.js
+++ app/js/app.js
@@ -4,10 +4,17 @@ window.App = {
   Collections: {},
 
   initialize: function() {
-    alert("Hello, from backbone!");
+    // alert("Hello, from backbone!");
   }
 };
 
 $(function() {
   App.initialize();
+
+  $.ajaxPrefilter("json", function(options) {
+    if (options.headers === undefined) {
+      options.headers = {};
+    }
+    options.headers['Accept'] = 'application/json';
+  });
 });
